ce2498d4cf4e44b40275f982021ab5573f5c7168
chakrapani priyadarshi <chakrapani.priyadarshi@mhealth.tech>
2023-01-02 16:30:51 +0530

    added Selnium

--- src/test/java/TestSelenium.java
+++ src/test/java/TestSelenium.java
@@ -1,3 +1,7 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class TestSelenium {
@@ -5,8 +9,30 @@ public class TestSelenium {
 
 
     @Test
-    public void test()
+    public void ChromeDriverSetUptest()
     {
         System.out.print("passed");
+        System.setProperty("webdriver.chrome.driver","/Users/Priyadarshi/Desktop/Selenium/src/server/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
+
+        //driver.findElement(By.id("twotabsearchtextbox")).click();
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("vivo s1 pro");
+        driver.findElement(By.id("nav-search-submit-button")).click();
+
+        driver.close();
+
+    }
+
+    @Test(enabled = false)
+    public void FirefoxDriverSetUptest()
+    {
+        System.out.print("passed");
+        System.setProperty("webdriver.gecko.driver","/Users/Priyadarshi/Desktop/Selenium/src/server/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.geeksforgeeks.org/count-occurrences-of-anagrams/");
+        driver.manage().window().maximize();
+        driver.close();
     }
 }
---
+++ src/test/java/test.java
@@ -0,0 +1,11 @@
+public class test {
+
+    public static void main(String args[])
+    {
+
+        System.out.print("pasdd   test");
+        int a=2;
+        int b=9;
+        System.out.print(a+b);
+    }
+}
